094fcd8df2f947fe1ea551e871a1455bbd4f3e36
Jordi Armengol Estapé <me@jordiae.com>
2016-11-16 14:40:20 +0100

    Fixed recyclerview

--- app/src/main/java/com/fgcalarm/CustomAdapter.java
+++ app/src/main/java/com/fgcalarm/CustomAdapter.java
@@ -42,25 +42,20 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.AdapterVie
 
         // hardcoded: test
         ArrayList<Estacio> estacions = new ArrayList<>();
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
+        estacions.add(new Estacio("Test"));
 
         linies = new ArrayList<>();
-        linies.add(new Linia(1,"S1",estacions));
-        linies.add(new Linia(1,"S1",estacions));
-        linies.add(new Linia(1,"S1",estacions));
-        linies.add(new Linia(1,"S1",estacions));
-        linies.add(new Linia(1,"S1",estacions));
-        linies.add(new Linia(1,"S1",estacions));
-        linies.add(new Linia(1,"S1",estacions));
-        linies.add(new Linia(1,"S1",estacions));
+        linies.add(new Linia(0,"CA7",estacions));
+        linies.add(new Linia(1,"R5",estacions));
+        linies.add(new Linia(2,"R50",estacions));
+        linies.add(new Linia(3,"R60",estacions));
+        linies.add(new Linia(4,"S1",estacions));
+        linies.add(new Linia(5,"S2",estacions));
+        linies.add(new Linia(6,"S33",estacions));
+        linies.add(new Linia(7,"S4",estacions));
+        linies.add(new Linia(8,"S5",estacions));
+        linies.add(new Linia(9,"S55",estacions));
+        linies.add(new Linia(10,"S8",estacions));
     }
 
 
@@ -75,8 +70,38 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.AdapterVie
     public void onBindViewHolder(CustomAdapter.AdapterViewHolder adapterViewholder, int position) {
         int iconLayout = linies.get(position).getIcon();
         switch (iconLayout){
+            case 0:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.ca7));
+                break;
             case 1:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.r50));
+                break;
+            case 2:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.r6));
+                break;
+            case 3:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.r60));
+                break;
+            case 4:
                 adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.s1));
+                break;
+            case 5:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.s2));
+                break;
+            case 6:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.s33));
+                break;
+            case 7:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.s4));
+                break;
+            case 8:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.s5));
+                break;
+            case 9:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.s55));
+                break;
+            case 10:
+                adapterViewholder.icon.setImageDrawable(adapterViewholder.v.getResources().getDrawable(R.drawable.s8));
                 break;
         }
         //adapterViewholder.name.setText(linies.get(position).getName());
