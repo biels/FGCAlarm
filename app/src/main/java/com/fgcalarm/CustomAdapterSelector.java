99292220a71282299c5be01ea3d14cf77d03ca33
Jordi Armengol Estapé <me@jordiae.com>
2016-11-16 14:40:20 +0100

    Fixed recyclerview

--- app/src/main/java/com/fgcalarm/CustomAdapterSelector.java
+++ app/src/main/java/com/fgcalarm/CustomAdapterSelector.java
@@ -2,25 +2,29 @@ package com.fgcalarm;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
+import java.util.Arrays;
 
-import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static com.fgcalarm.R.drawable.s1;
 
 
-public class CustomAdapterSelectorEstacions extends RecyclerView.Adapter<CustomAdapterSelectorEstacions.AdapterViewHolder>{
+public class CustomAdapterSelector extends RecyclerView.Adapter<CustomAdapterSelector.AdapterViewHolder>{
 
-    ArrayList<Estacio> estacions = new ArrayList<>();
+    ArrayList<Estacio> estacions;
     Context ctx;
+    Intent intent;
 
-    CustomAdapterSelectorEstacions(Context context){
+    CustomAdapterSelector(Context context){
         ctx = context;
+        intent = new Intent(ctx, ActivadorActivity.class);
 
         /* TODO: DB
         LoginDataBaseAdapter loginDataBaseAdapter;
@@ -38,28 +42,23 @@ public class CustomAdapterSelectorEstacions extends RecyclerView.Adapter<CustomA
 
         // hardcoded: test
 
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
-        estacions.add(new Estacio("Vallvidrera"));
+
+        estacions = new ArrayList<>();
+        estacions.add(new Estacio("Estació 1"));
+        estacions.add(new Estacio("Estació 2"));
     }
 
 
     @Override
-    public CustomAdapterSelectorEstacions.AdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+    public CustomAdapterSelector.AdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
         LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
         View view = inflater.inflate(R.layout.row_layout_selector, viewGroup, false);
         return new AdapterViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(CustomAdapterSelectorEstacions.AdapterViewHolder adapterViewholder, int position) {
-        adapterViewholder.name.setText(estacions.get(position).getName());
+    public void onBindViewHolder(CustomAdapterSelector.AdapterViewHolder adapterViewholder, int position) {
+       adapterViewholder.name.setText(estacions.get(position).getName());
 
     }
 
@@ -73,6 +72,7 @@ public class CustomAdapterSelectorEstacions extends RecyclerView.Adapter<CustomA
     public class AdapterViewHolder extends RecyclerView.ViewHolder {
 
 
+        public ImageView icon;
         public TextView name;
         public View v;
         public AdapterViewHolder(View itemView) {
@@ -80,12 +80,12 @@ public class CustomAdapterSelectorEstacions extends RecyclerView.Adapter<CustomA
             final int x = itemView.getVerticalScrollbarPosition();
             this.v = itemView;
             this.name = (TextView) itemView.findViewById(R.id.name);
-            itemView.setOnClickListener(new View.OnClickListener() {
+            this.name.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    final Intent intent = new Intent(ctx, ActivadorActivity.class);
+
                     intent.putExtra("estacio", estacions.get(x).getName());
-                    intent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     ctx.startActivity(intent);
                 }
             });
