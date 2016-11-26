d0ed9d08cb0afa4bc7324de89384aabea67eb3ba
Biel Simon Rojas <bielsimon2@gmail.com>
2016-11-26 22:14:33 +0100

    Improved NotImplementedException.java to provide reason message

--- app/src/main/java/com/fgcalarm/exceptions/NotImplementedException.java
+++ app/src/main/java/com/fgcalarm/exceptions/NotImplementedException.java
@@ -6,4 +6,18 @@ package com.fgcalarm.exceptions;
 
 public class NotImplementedException extends UnsupportedOperationException {
 
+    public NotImplementedException() {
+    }
+
+    public NotImplementedException(String message) {
+        super(message);
+    }
+
+    public NotImplementedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotImplementedException(Throwable cause) {
+        super(cause);
+    }
 }
