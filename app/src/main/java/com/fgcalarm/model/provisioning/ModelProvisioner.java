aad2c2e1b388253bc75c2692a71ecc873470ffe6
Biel Simon Rojas <bielsimon2@gmail.com>
2016-11-26 20:45:40 +0100

    Outlined provisioning system

---
+++ app/src/main/java/com/fgcalarm/model/provisioning/ModelProvisioner.java
@@ -0,0 +1,9 @@
+package com.fgcalarm.model.provisioning;
+
+/**
+ * Created by Biel on 26/11/2016.
+ */
+
+public abstract class ModelProvisioner {
+    abstract void provision();
+}
---
+++ app/src/main/java/com/fgcalarm/model/provisioning/ProvisioningManager.java
@@ -0,0 +1,9 @@
+package com.fgcalarm.model.provisioning;
+
+/**
+ * Created by Biel on 26/11/2016.
+ */
+
+public class ProvisioningManager {
+
+}
