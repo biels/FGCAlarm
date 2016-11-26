d20426ece085528aa58191c443bfcb93665c9406
Biel Simon Rojas <bielsimon2@gmail.com>
2016-11-26 09:53:09 +0100

    Updated Entity.java to handle id via generics

--- app/src/main/java/com/fgcalarm/model/entities/base/Entity.java
+++ app/src/main/java/com/fgcalarm/model/entities/base/Entity.java
@@ -1,8 +1,26 @@
 package com.fgcalarm.model.entities.base;
 
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+
 /**
  * Created by Biel on 22/11/2016.
  */
 
-public abstract class Entity {
+public abstract class Entity<ID extends Serializable> {
+    @NonNull
+    private ID id;
+
+    public Entity(@NonNull ID id) {
+        this.id = id;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
 }
